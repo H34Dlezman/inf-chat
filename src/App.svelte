424bbd4f7cf6921b18ed3e285416999ae8f4055f
headless <parsa@pamagames.de>
2022-07-22 00:28:02 +0200

    scrolldown on msg timeout

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
     console.log("NEW Message:", msgs);
     messages = msgs
     loaded = true
-    document.getElementById("MessageList").scrollTop=document.getElementById("MessageList").scrollHeight
+    setTimeout(()=>{document.getElementById("MessageList").scrollTop=document.getElementById("MessageList").scrollHeight}, 500)
   })
 
   const sendMessage = () => {
