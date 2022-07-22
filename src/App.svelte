0c5a96f50b94407fb86ce95dd753fcf5f7c70fdc
headless <parsa@pamagames.de>
2022-07-22 03:53:22 +0200

    make update

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
     //console.log("NEW Message:", msgs);
     messages = msgs
     loaded = true
-    //setTimeout(()=>{document.getElementById("MessageList").scroll(0,0)}, 50)
+    setTimeout(()=>{document.getElementById("MessageList").scroll(0,0)}, 50)
   })
 
   const sendMessage = () => {
